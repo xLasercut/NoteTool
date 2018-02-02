bd4534a3f21a27476f2a3a7ef3736b3a8a79a5fe
Lasercut <tianxun.wang2004@gmail.com>
2018-02-02 16:30:12 +0000

    added new note function

--- js/btn-functions.js
+++ js/btn-functions.js
@@ -10,10 +10,26 @@ function open_add_window () {
         protocol: 'file:',
         slashes: true
     }));
+
+    // Open the DevTools.
+    add_window.webContents.openDevTools()
+
+    // Emitted when the window is closed.
+    add_window.on('closed', function () {
+        // Dereference the window object, usually you would store windows
+        // in an array if your app supports multi windows, this is the time
+        // when you should delete the corresponding element.
+        add_window = null
+    })
 }
 
 
 function add_new_note () {
     var message = document.getElementById("addnote_text");
-    console.log(message.value);
+    var title = document.getElementById("addnote_title");
+    obj = {
+        title: title.value,
+        message: message.value
+    };
+    console.log(JSON.stringify(obj));
 }
