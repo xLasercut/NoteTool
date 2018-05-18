be8f90baba447bdf62d1c817e08e6cf6451f0c10
xLasercut <tianxun.wang2004@gmail.com>
2018-05-18 16:36:25 +0100

    beautified editor buttons

--- src/script/format-manager.js
+++ src/script/format-manager.js
@@ -1,26 +1,109 @@
-class FormatManager {
-    constructor () {
-        this.headers = ["h1", "h2", "h3"]
-        this.fontStyle = ["bold", "italic", "strikethrough", "underline", "justifyCenter", "justifyFull", "justifyLeft", "justifyRight"]
+const headers = [
+    {
+        tag: "h1",
+        value: "<b>H1</b>"
+    },
+    {
+        tag: "h2",
+        value: "<b>H2</b>"
+    },
+    {
+        tag: "h3",
+        value: "<b>H3</b>"
     }
+]
 
+const fontStyle = [
+    {
+        tag: "bold",
+        value: "<i class='fas fa-bold'></i>"
+    },
+    {
+        tag: "italic",
+        value: "<i class='fas fa-italic'></i>"
+    },
+    {
+        tag: "strikethrough",
+        value: "<i class='fas fa-strikethrough'></i>"
+    },
+    {
+        tag: "underline",
+        value: "<i class='fas fa-underline'></i>"
+    }
+]
+
+const alignment = [
+    {
+        tag: "justifyFull",
+        value: '<i class="fas fa-align-justify"></i>'
+    },
+    {
+        tag: "justifyLeft",
+        value: '<i class="fas fa-align-left"></i>'
+    },
+    {
+        tag: "justifyRight",
+        value: '<i class="fas fa-align-right"></i>'
+    },
+    {
+        tag: "justifyCenter",
+        value: '<i class="fas fa-align-center"></i>'
+    },
+    {
+        tag: "insertUnorderedList",
+        value: '<i class="fas fa-list-ul"></i>'
+    },
+    {
+        tag: "insertOrderedList",
+        value: '<i class="fas fa-list-ol"></i>'
+    }
+]
+
+const special = [
+    {
+        tag: "createLink",
+        value: '<i class="fas fa-link"></i>'
+    },
+    {
+        tag: "insertImage",
+        value: '<i class="far fa-image"></i>'
+    }
+]
+
+function sortArray (array) {
+    return array.sort(function (a, b) {
+        return a.tag > b.tag
+    })
+}
+
+class FormatManager {
     addFormat (tag) {
-        if (this.headers.indexOf(tag) > -1) {
-            document.execCommand("formatBlock", false, tag)
+        for (var item of headers) {
+            if (tag == item.tag) {
+                return document.execCommand("formatBlock", false, tag)
+            }
         }
 
-        if (this.fontStyle.indexOf(tag) > -1) {
-            document.execCommand(tag, false, null)
+        for (var item of fontStyle.concat(alignment)) {
+            if (tag == item.tag) {
+                return document.execCommand(tag, false, null)
+            }
+        }
+
+        for (var item of special) {
+            if (tag == item.tag) {
+                var url = ""
+                return document.execCommand(tag, false, url)
+            }
         }
     }
 
     getBtns () {
         var formatBtns = []
-        formatBtns = formatBtns.concat(this.headers)
-        formatBtns = formatBtns.concat(this.fontStyle)
-        formatBtns = formatBtns.sort(function (a, b) {
-            return a > b
-        })
+        formatBtns = formatBtns.concat(sortArray(special))
+        formatBtns = formatBtns.concat(sortArray(headers))
+        formatBtns = formatBtns.concat(sortArray(alignment))
+        formatBtns = formatBtns.concat(sortArray(fontStyle))
         return formatBtns
     }
 }
